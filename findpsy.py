0c5f581668940eb7592d342b5d1bf6671255d5b8
AlexVanin <opertm@gmail.com>
2016-03-17 17:58:52 +0300

    Fixed bug in search algorythm

--- findpsy.py
+++ findpsy.py
@@ -19,29 +19,28 @@ start = 0;
 all_paths = []
 
 def deep (path, result):
-	for i in range(path[-1], len(matrix[0])):
-		if matrix[path[-1]][i] == 0:
+
+	if all(v > 0 for v in result):
+		print "PATH: {0}".format(path)
+		all_paths.append(path)
+		return 1
+
+	for i in range(path[-1], len(result)):
+		if result[i] == 0:
 			new_path = list(path)
 			new_result = list(result)
-
 			new_path.append(i)
 
 			for j in range(0, len(new_result)):
 				new_result[j] = new_result[j] | matrix[i][j]
 
-			if all(v > 0 for v in new_result):
-				print "PATH: {0}".format(new_path)
-				all_paths.append(new_path)
-				return 1
-			else:
-				deep (new_path, new_result)
+			deep (new_path, new_result)
 
 	return 0
 
 
 f = open (sys.argv[1])
 matrix = [ map(int,line.split(';')) for line in f ]
-
 
 while True:
 	my_path = [ start ]
